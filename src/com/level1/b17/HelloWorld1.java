324f9b7a2c36fa93dbf23c655d7491fb4da64a30
Oleksandr Kuzmenko <okuzmenkoa@gmail.com>
2018-04-05 14:15:48 +0300

    Create package com.level1.b17 and class HelloWord1

---
+++ src/com/level1/b17/HelloWorld1.java
@@ -0,0 +1,8 @@
+package com.level1.b17;
+
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Create by alexax");
+    }
+}

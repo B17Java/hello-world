5a96db0f1decc3a5d3b82c653f87886ce938ea49
Oleksandr Kuzmenko <okuzmenkoa@gmail.com>
2018-04-06 13:01:57 +0300

    Update HelloWorld

--- src/com/level1/b17/HelloWorld1.java
+++ src/com/level1/b17/HelloWorld1.java
@@ -1,13 +1,10 @@
 package com.level1.b17;
-
 import com.level3.b17.Creater;
-
 public class HelloWorld1 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Create by alexax");
-        System.out.println("I added myself");
+
         Creater.make();
 
-    }
+}
 }

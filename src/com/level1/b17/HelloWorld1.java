c8ca1affb8202677151f0610fcf23e53b86ec7ce
Volodymyr <vlad5855051@gmail.com>
2018-04-07 15:47:56 +0300

    Merge branch 'master' of F:\ITA\JAVA\hello-world with conflicts.

--- src/com/level1/b17/HelloWorld1.java
+++ src/com/level1/b17/HelloWorld1.java
@@ -1,13 +1,12 @@
 package com.level1.b17;
-import com.level3.b17.Creater;
+
 public class HelloWorld1 {
     public static void main(String[] args) {
-        System.out.println("Introduce yourself!");
+        System.out.println("Hello world!");
+        System.out.println("it`s boaring, guys");
         Creater.make();
-        Human st1 = new Human("Oleksandr", "Kuzmenko");
-        st1.setEmail("okuzmenkoa@gmail.com");
-        System.out.println(st1);
 
-
-
+        NewClassToTest.my();
+        }
+}
 }

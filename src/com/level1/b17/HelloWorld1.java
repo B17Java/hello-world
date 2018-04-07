31c09c78bfd6bc0cf6652204acfd2ef1c0ba986c
Alex <alexey8800@gmail.com>
2018-04-07 15:26:25 +0300

    First AlexSh's method

--- src/com/level1/b17/HelloWorld1.java
+++ src/com/level1/b17/HelloWorld1.java
@@ -1,4 +1,5 @@
 package com.level1.b17;
+import com.level2.b17.NewClassToTest;
 import com.level3.b17.Creater;
 public class HelloWorld1 {
     public static void main(String[] args) {
@@ -6,5 +7,6 @@ public class HelloWorld1 {
 
         Creater.make();
 
-}
+        NewClassToTest.my();
+        }
 }
---
+++ src/com/level2/b17/NewClassToTest.java
@@ -0,0 +1,7 @@
+package com.level2.b17;
+
+public class NewClassToTest {
+    public static void my() {
+        System.out.println("!");
+    }
+}

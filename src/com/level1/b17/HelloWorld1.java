d80f3c45c5394661d33ef95a633da1933a5ddcad
Iryna Pfeiffer <iryna.pfeiffer@gmail.com>
2018-04-07 18:29:08 +0300

    new class has been added

--- src/com/level1/b17/HelloWorld1.java
+++ src/com/level1/b17/HelloWorld1.java
@@ -1,5 +1,7 @@
 package com.level1.b17;
 import com.level3.b17.Creater;
+import com.level4.b17.Iryna;
+
 public class HelloWorld1 {
     public static void main(String[] args) {
         System.out.println("Introduce yourself!");
@@ -11,9 +13,14 @@ public class HelloWorld1 {
         Human st2 = new Human("Aleksey", "Shchehlov");
         st2.setEmail("alexey8800@gmail.com");
         st2.setPhone("+380931685958");
+        System.out.println(st2);
         Human st3 = new Human("Volodymyr", "Grinchenko");
         st3.setEmail("vlad5855051@gmail.com");
         System.out.println(st3);
+        Human st4 = new Human("Iryna", "Pfeiffer");
+        st4.setEmail("iryna.pfeiffer@gmail.com");
+        System.out.println(st4);
+        Iryna.Say();
 
     }
 }
---
+++ src/com/level4/b17/Iryna.java
@@ -0,0 +1,7 @@
+package com.level4.b17;
+
+public class Iryna {
+    public static void Say () {
+        System.out.println("Hi there!");
+    }
+}
